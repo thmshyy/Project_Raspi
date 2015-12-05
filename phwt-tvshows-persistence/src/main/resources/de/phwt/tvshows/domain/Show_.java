db68997a0791adab45603ee38256e0e1cb68bc73
Viviane Whyte <vi.whyte@web.de>
2015-12-05 16:50:13 +0100

    zhgk

---
+++ phwt-tvshows-persistence/src/main/resources/de/phwt/tvshows/domain/Show_.java
@@ -0,0 +1,13 @@
+package de.phwt.tvshows.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-12-04T22:51:08.916+0100")
+@StaticMetamodel(Show.class)
+public class Show_ {
+	public static volatile SingularAttribute<Show, Long> id;
+	public static volatile SingularAttribute<Show, Integer> staffel;
+	public static volatile SingularAttribute<Show, String> name;
+}
