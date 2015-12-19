a74b46471532b43a733579dad133bc7880c3402d
thmshyy <thomas.weit@hotmail.de>
2015-12-19 12:20:48 +0100

    RESTClient.java cleaned

--- phwt-tvshows-webClient/src/main/java/de/phwt/tvshows/webClient/RESTClient.java
+++ phwt-tvshows-webClient/src/main/java/de/phwt/tvshows/webClient/RESTClient.java
@@ -4,10 +4,8 @@ import java.net.URI;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.client.ClientConfig;
@@ -16,27 +14,6 @@ public class RESTClient
 {
 
 	private static final String webServiceURI = "http://localhost:8080/phwt-tvshows-web";
-
-	public static void main(final String[] args)
-	{
-		final ClientConfig clientConfig = new ClientConfig();
-		final Client client = ClientBuilder.newClient(clientConfig);
-		final URI serviceURI = UriBuilder.fromUri(webServiceURI).build();
-		final WebTarget webTarget = client.target(serviceURI);
-
-		// response
-		System.out.println(webTarget.path("rest").path("ShowService").path("getShows").request().accept("application/json").get(Response.class).toString());
-
-		// json
-		//System.out.println(webTarget.path("rest").path("ShowService").path("getShows").request().accept("application/json"));
-
-		// json
-		//System.out.println(webTarget.path("rest").path("ShowService").path("getShows").request().accept(MediaType.APPLICATION_JSON).get(String.class));
-		final String response = webTarget.path("rest").path("ShowService").path("getShows").request().accept(MediaType.APPLICATION_JSON).get(String.class);
-		final Builder response1 = webTarget.path("rest").path("ShowService").path("getShows").request().accept(MediaType.APPLICATION_JSON);
-		System.out.println(response);
-		//System.out.println(response1);
-	}
 
 	public String getList()
 	{
@@ -50,8 +27,5 @@ public class RESTClient
 
 		return response;
 	}
-	/*private convertToHTML()
-	{
-		return;
-	}*/
+
 }
